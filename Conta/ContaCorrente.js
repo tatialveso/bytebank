260ecfc3a20b05123458cde6bd6a5bd0c0a2b933
tatialveso <tatiialveso@gmail.com>
2021-01-04 19:11:13 -0300

    bytebank

---
+++ Conta/ContaCorrente.js
@@ -0,0 +1,15 @@
+import { Conta } from './Conta.js';
+
+export class ContaCorrente extends Conta {
+    static numContas = 0;
+
+    constructor(cliente, agencia) {
+        super(0, cliente, agencia);
+        ContaCorrente.numContas += 1;
+    }
+
+    sacar(valor) {
+        let taxa = 1.1;
+        return this._sacar(valor, taxa);
+    }
+}
